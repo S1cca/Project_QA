d20dbb90e89b90c0f727605ecaa3325410348351
Wei Yao <s1cca@outlook.com>
2022-04-04 14:42:19 +0000

    model added

---
+++ application/model.py
@@ -0,0 +1,21 @@
+from app import db
+
+
+class Current_Account(db.model):
+    C_A_id = db.Column(db.Integer, primary_key=True)
+    balance = db.Column(db.Float)
+    account_number = db.Column(db.Integer)
+    account_type = db.Column(db.String(20))
+    account_status = db.Column(db.String(20))
+    account_open_date = db.Column(db.Date)
+
+
+
+class Saving_Account(db.model):
+    S_A_id = db.Column(db.Integer, primary_key=True)
+    balance = db.Column(db.Float)
+    account_number = db.Column(db.Integer)
+    account_type = db.Column(db.String(20))
+    account_status = db.Column(db.String(20))
+    account_open_date = db.Column(db.Date)
+
