99736c76d4c5a31f957340ae2defde8938b59dd1
Wei Yao <s1cca@outlook.com>
2022-04-05 22:16:50 +0000

    complete model

--- application/model.py
+++ application/model.py
@@ -7,6 +7,12 @@ class Game(db.Model):
     publisher = db.Column(db.String(200), nullable=False)
     release_date = db.Column(db.Date, nullable=False)
 
+    def __init__(self, game, mode, publisher, release_date):
+        self.game = game
+        self.mode = mode
+        self.publisher = publisher
+        self.release_date = release_date
+
 
 class Review(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -14,5 +20,11 @@ class Review(db.Model):
     review_date = db.Column(db.Date, nullable=False)
     rating = db.Column(db.Integer)
     comments = db.Column(db.String(200), nullable=False)
+
+    def __init__(self, game_id, review_date, rating, comments):
+        self.game_id = game_id
+        self.review_date = review_date
+        self.rating = rating
+        self.comments = comments
 
 
